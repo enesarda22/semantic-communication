c65c426231882061cfeaa4a083cecf1079aa3cc1
KutayEmrecan <emrecankutay@gmail.com>
2023-10-24 14:22:12 -0400

    Channel Train Update

--- channel_train_test.py
+++ channel_train_test.py
@@ -19,66 +19,18 @@ if __name__ == "__main__":
     data_handler = DataHandler(device=device)
     data_handler.load_data()
 
-    # TX - RX
-    class tx_rx(nn.Module):
-        def __init__(self, SNR, sig_pow):
-            super(tx_rx, self).__init__()
-
-            self.encoder = Channel_Encoder(364, 128)
-            self.decoder = Channel_Decoder(128,364)
-            self.channel = AWGN(SNR, sig_pow)
-
-        def forward(self, x):
-            return self.decoder(self.channel(self.encoder(x)))
-
-    tx_rx_model = tx_rx(1, 1)
-    optimizer = torch.optim.AdamW(tx_rx_model.parameters(), lr=1e-4)
-    criterion = nn.MSELoss()
-
-    for _ in range(10):
-        train_losses = []
-        tx_rx_model.train()
-        for b in tqdm(data_handler.train_dataloader):
-            xb = b[0].to(device)
-            encoder_output = b[1].to(device)
-
-            model_output = tx_rx_model(encoder_output)
-
-            loss = criterion(model_output, encoder_output)
-            optimizer.zero_grad(set_to_none=True)
-            loss.backward()
-            optimizer.step()
-            train_losses.append(loss.item())
-
-        val_losses = []
-        tx_rx_model.eval()
-        for b in data_handler.val_dataloader:
-            xb = b[0].to(device)
-            encoder_output = b[1].to(device)
-
-            model_output = tx_rx_model(encoder_output)
-            loss = criterion(model_output, encoder_output)
-            val_losses.append(loss.item())
-
-        print("\n")
-        print_loss(train_losses, "Train")
-        print_loss(val_losses, "Val")
-
-    # torch.save(model.state_dict(), "tx_rx_model.pt")
-
     # TX - RELAY
     class tx_relay(nn.Module):
         def __init__(self, SNR, sig_pow):
             super(tx_relay, self).__init__()
 
-            self.encoder = Channel_Encoder(364, 128)
-            self.encoder.load_state_dict(tx_rx_model.encoder.state_dict())  # load encoder of TX
+            self.tx_encoder = Channel_Encoder(364, 128)
 
-            self.decoder = Channel_Decoder(128, 364)
+            self.relay_decoder = Channel_Decoder(128, 364)
             self.channel = AWGN(SNR, sig_pow)
 
         def forward(self, x):
-            return self.decoder(self.channel(self.encoder(x)))
+            return self.relay_decoder(self.channel(self.tx_encoder(x)))
 
 
     tx_relay_model = tx_relay(1, 1)
@@ -116,32 +68,40 @@ if __name__ == "__main__":
 
     # torch.save(model.state_dict(), "tx_relay_model.pt")
 
-    # RELAY - RX
-    class relay_rx(nn.Module):
-        def __init__(self, SNR, sig_pow):
-            super(relay_rx, self).__init__()
+    # TX-ENC, RELAY-ENC - RX-DEC
+    class tx_relay_rx(nn.Module):
+        def __init__(self, SNR_TX_RX, SNR_REL_RX, sig_pow_TX_RX, sig_pow_REL_RX):
+            super(tx_relay_rx, self).__init__()
 
-            self.encoder = Channel_Encoder(364, 128)
+            self.tx_encoder = Channel_Encoder(364, 128)
+            self.tx_encoder.load_state_dict(tx_relay_model.tx_encoder.state_dict())  # load decoder of TX enc
 
-            self.encoder.load_state_dict(tx_rx_model.decoder.state_dict())  # load decoder of RX
-            self.channel = AWGN(SNR, sig_pow)
+            self.relay_encoder = Channel_Encoder(364, 128)
 
-        def forward(self, x):
-            return self.decoder(self.channel(self.encoder(x)))
+            self.rx_decoder = Channel_Decoder(128, 364)
+
+            self.channel_tx_rx = AWGN(SNR_TX_RX, sig_pow_TX_RX)
+            self.channel_rel_rx = AWGN(SNR_REL_RX, sig_pow_REL_RX)
+
+        def forward(self, x1, x2):
+            y1 = self.channel_tx_rx(self.tx_encoder(x1))
+            y2 = self.channel_rel_rx(self.relay_encoder(x2))
+
+            return self.rx_decoder(y1+y2)
 
 
-    relay_rx_model = relay_rx(1, 1)
-    optimizer = torch.optim.AdamW(relay_rx_model.parameters(), lr=1e-4)
+    tx_relay_rx_model = tx_relay_rx(1, 1,1, 1)
+    optimizer = torch.optim.AdamW(tx_relay_rx_model.parameters(), lr=1e-4)
     criterion = nn.MSELoss()
 
     for _ in range(10):
         train_losses = []
-        relay_rx_model.train()
+        tx_relay_rx_model.train()
         for b in tqdm(data_handler.train_dataloader):
             xb = b[0].to(device)
             encoder_output = b[1].to(device)
 
-            model_output = relay_rx_model(encoder_output)
+            model_output = tx_relay_rx_model(encoder_output)
 
             loss = criterion(model_output, encoder_output)
             optimizer.zero_grad(set_to_none=True)
@@ -150,12 +110,12 @@ if __name__ == "__main__":
             train_losses.append(loss.item())
 
         val_losses = []
-        relay_rx_model.eval()
+        tx_relay_rx_model.eval()
         for b in data_handler.val_dataloader:
             xb = b[0].to(device)
             encoder_output = b[1].to(device)
 
-            model_output = relay_rx_model(encoder_output)
+            model_output = tx_relay_rx_model(encoder_output)
             loss = criterion(model_output, encoder_output)
             val_losses.append(loss.item())
 
