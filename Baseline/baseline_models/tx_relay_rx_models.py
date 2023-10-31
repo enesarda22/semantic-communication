433e52aa1264319b646531e4e696b9f6c297227a
KutayEmrecan <emrecankutay@gmail.com>
2023-10-30 20:19:13 -0400

    baseline code

---
+++ Baseline/baseline_models/tx_relay_rx_models.py
@@ -0,0 +1,81 @@
+from torch import nn
+from utils.channel import Channel
+from baseline_models.channel_enc_dec_blocks import ChannelEncoder, ChannelDecoder
+import torch
+import torch.nn.functional as F
+
+
+class Tx_Relay(nn.Module):
+    def __init__(self, nin, n_emb, n_latent, channel: Channel, entire_network_train=0):
+        super(Tx_Relay, self).__init__()
+
+        self.embedding_layer = nn.Embedding(nin, n_emb)
+        self.tx_encoder = ChannelEncoder(n_emb, n_latent)
+        self.relay_decoder = ChannelDecoder(n_latent, n_emb)
+        self.linear = nn.Linear(n_emb, nin)
+        self.channel = channel
+        self.entire_network_train = entire_network_train
+
+    def forward(self, x, attention_mask):
+        embeddings = self.embedding_layer(x)
+        ch_input = self.tx_encoder(embeddings)
+        ch_output = self.channel(ch_input)
+        x_hat = self.linear(self.relay_decoder(ch_output))
+
+        if self.entire_network_train == 0:
+            B, T, C = x_hat.shape
+            logits = x_hat.reshape(B * T, C)
+            grnd_x = x.reshape(B * T)
+            attention_mask = attention_mask.flatten() == 1
+
+            loss = F.cross_entropy(
+                logits[attention_mask, :], grnd_x[attention_mask]
+            )
+
+            return x_hat, ch_input, loss
+
+        else:
+            return x_hat, ch_input
+
+
+class Tx_Relay_Rx(nn.Module):
+    def __init__(self, nin, n_emb, n_latent, tx_rx_channel: Channel, relay_rx_channel: Channel,
+                 tx_relay_model: Tx_Relay):
+        super(Tx_Relay_Rx, self).__init__()
+
+        self.tx_relay_model = tx_relay_model
+        # Freeze
+        for param in self.tx_relay_model.parameters():
+            param.requires_grad = False
+
+        self.relay_embedding = nn.Embedding(nin, n_emb)
+        self.relay_encoder = ChannelEncoder(n_emb, n_latent)
+        self.rx_decoder_1 = ChannelDecoder(n_latent, n_emb)
+        self.rx_decoder_2 = ChannelDecoder(n_latent, n_emb)
+
+        self.linear = nn.Linear(int(2 * n_emb), nin)
+        self.tx_rx_channel = tx_rx_channel
+        self.relay_rx_channel = relay_rx_channel
+
+    def forward(self, x, attention_mask):
+        x_hat, x1 = self.tx_relay_model(x, attention_mask)
+
+        x_hard = torch.argmax(x_hat, dim=2)
+
+        x_emb = self.relay_embedding(x_hard)
+
+        y2 = self.relay_rx_channel(self.relay_encoder(x_emb))
+        y1 = self.tx_rx_channel(x1)
+
+        x_hat = torch.cat((self.rx_decoder_1(y1), self.rx_decoder_2(y2)), dim=2)
+        x_hat = self.linear(x_hat)
+
+        B, T, C = x_hat.shape
+        logits = x_hat.reshape(B * T, C)
+        grnd_x = x.reshape(B * T)
+        attention_mask = attention_mask.flatten() == 1
+
+        loss = F.cross_entropy(
+            logits[attention_mask, :], grnd_x[attention_mask]
+        )
+        return x_hat, loss
---
+++ Baseline/utils/general.py
@@ -0,0 +1,35 @@
+import os
+import random
+from pathlib import Path
+
+import torch
+import numpy as np
+
+RANDOM_STATE = 42
+
+
+def set_seed():
+    random.seed(RANDOM_STATE)
+    torch.manual_seed(RANDOM_STATE)
+    torch.cuda.manual_seed(RANDOM_STATE)
+    np.random.seed(RANDOM_STATE)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmarks = False
+    torch.autograd.set_detect_anomaly(True)
+
+
+def get_device():
+    return torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
+def print_loss(losses, group):
+    mean_loss = np.mean(losses)
+    se = np.std(losses, ddof=1) / np.sqrt(len(losses))
+    print(f"{group} Mean Loss: {mean_loss:.3f} ± {se:.3f}")
+
+
+def create_checkpoint(path, **kwargs):
+    p = Path(path)
+    p.parent.mkdir(parents=True, exist_ok=True)
+
+    torch.save({**kwargs}, p)
