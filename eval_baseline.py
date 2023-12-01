0f873e71b85950f8424c8dbb73c4b67adc141811
KutayEmrecan <72248203+KutayEmrecan@users.noreply.github.com>
2023-12-01 12:31:48 -0500

    update eval code for baseline

--- eval_baseline.py
+++ eval_baseline.py
@@ -23,7 +23,6 @@ def semantic_similarity_score(target_sentences, received_sentences):
     target_emb = semantic_encoder(messages=target_sentences)
     received_emb = semantic_encoder(messages=received_sentences)
     scores = F.cosine_similarity(target_emb, received_emb)
-
     return scores
 
 
@@ -57,10 +56,10 @@ if __name__ == "__main__":
     # test args
     parser.add_argument("--batch-size", default=32, type=int)
     parser.add_argument("--gamma-list", nargs="+", type=float)
-    parser.add_argument("--d", type=float)
+    parser.add_argument("--d-list", nargs="+", type=float)
+    parser.add_argument("--n-test", default=20000, type=int)
 
     args = parser.parse_args()
-
     device = get_device()
     set_seed()
 
@@ -93,134 +92,149 @@ if __name__ == "__main__":
     ).to(device)
     load_model(tx_relay_rx_model, args.tx_relay_rx_path)
 
-    semantic_sim = []
-    bleu_1 = []
-    bleu_2 = []
-    bleu_3 = []
-    bleu_4 = []
+    mean_semantic_sim = np.zeros((len(args.d_list), len(args.gamma_list)))
+    mean_bleu_1 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    mean_bleu_2 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    mean_bleu_3 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    mean_bleu_4 = np.zeros((len(args.d_list), len(args.gamma_list)))
 
-    d_sd = args.d
 
-    for gamma in args.gamma_list:
-        print("Simulating for distance: " + str(gamma * args.d))
+    std_semantic_sim = np.zeros((len(args.d_list), len(args.gamma_list)))
+    std_bleu_1 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    std_bleu_2 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    std_bleu_3 = np.zeros((len(args.d_list), len(args.gamma_list)))
+    std_bleu_4 = np.zeros((len(args.d_list), len(args.gamma_list)))
 
-        cosine_scores = []
-        bleu1_scores = []
-        bleu2_scores = []
-        bleu3_scores = []
-        bleu4_scores = []
+    # For each d_sd
+    for distance_index, d_sd in enumerate(args.d_list):
 
-        d_sr = d_sd * gamma
-        d_rd = d_sd - d_sr
+        # For each gamma in gamma list
+        for gamma_index, gamma in enumerate(args.gamma_list):
+            print(f"Simulating for distance: {d_sd}  - Gamma: {gamma}")
 
-        tx_relay_rx_model.eval()
-        for b in data_handler.test_dataloader:
-            xb = b[0].to(device)
-            attention_mask = b[1].to(device)
-            xb = data_handler.encode_token_ids(xb)
+            cosine_scores = []
+            bleu1_scores = []
+            bleu2_scores = []
+            bleu3_scores = []
+            bleu4_scores = []
 
-            B, T = xb.shape
-            with torch.no_grad():
-                logits, _ = tx_relay_rx_model(
-                    xb[:, 1:], attention_mask[:, 1:], d_sd, d_sr, d_rd
-                )
-                probs = F.softmax(logits, dim=-1)
-                predicted_ids = (torch.argmax(probs, dim=-1)).reshape(
-                    B, args.max_length
-                )
+            d_sr = d_sd * gamma
+            d_rd = d_sd - d_sr
 
-                end_token_id = data_handler.encoder.transform([102])[0]
-                end_prediction_idx = torch.argmax(
-                    predicted_ids.eq(end_token_id).double(), dim=1
-                )
+            tx_relay_rx_model.eval()
+            for b in data_handler.test_dataloader:
+                xb = b[0].to(device)
+                attention_mask = b[1].to(device)
+                xb = data_handler.encode_token_ids(xb)
 
-                # zero means no end token prediction
-                end_prediction_idx[end_prediction_idx == 0] = T - 1
+                B, T = xb.shape
+                with torch.no_grad():
+                    logits, _ = tx_relay_rx_model(
+                        xb[:, 1:], attention_mask[:, 1:], d_sd, d_sr, d_rd
+                    )
+                    probs = F.softmax(logits, dim=-1)
+                    predicted_ids = (torch.argmax(probs, dim=-1)).reshape(
+                        B, args.max_length
+                    )
 
-                # prediction mask is created based on end token predictions
-                pred_mask = (torch.arange(T - 1).to(device)).le(
-                    end_prediction_idx.view(-1, 1)
-                )
+                    end_token_id = data_handler.encoder.transform([102])[0]
+                    end_prediction_idx = torch.argmax(
+                        predicted_ids.eq(end_token_id).double(), dim=1
+                    )
 
-                predicted_sentences = data_handler.get_tokens(
-                    ids=predicted_ids,
-                    attention_mask=pred_mask,
-                    skip_special_tokens=True,
-                )
+                    # zero means no end token prediction
+                    end_prediction_idx[end_prediction_idx == 0] = T - 1
 
-                original_sentences = data_handler.get_tokens(
-                    ids=xb,
-                    attention_mask=attention_mask,
-                    skip_special_tokens=True,
-                )
+                    # prediction mask is created based on end token predictions
+                    pred_mask = (torch.arange(T - 1).to(device)).le(
+                        end_prediction_idx.view(-1, 1)
+                    )
 
-                for s1, s2 in zip(original_sentences, predicted_sentences):
-                    cosine_scores.append(semantic_similarity_score([s1], [s2])[0][0])
+                    predicted_sentences = data_handler.get_tokens(
+                        ids=predicted_ids,
+                        attention_mask=pred_mask,
+                        skip_special_tokens=True,
+                    )
 
-                    bleu1_scores.append(bleu_1gram(s1, s2))
-                    bleu2_scores.append(bleu_2gram(s1, s2))
-                    bleu3_scores.append(bleu_3gram(s1, s2))
-                    bleu4_scores.append(bleu_4gram(s1, s2))
-            if len(cosine_scores) > 5000:
-                break
+                    original_sentences = data_handler.get_tokens(
+                        ids=xb,
+                        attention_mask=attention_mask,
+                        skip_special_tokens=True,
+                    )
 
-        semantic_sim.append(np.mean([i.tolist() for i in cosine_scores]))
-        bleu_1.append(np.mean(bleu1_scores))
-        bleu_2.append(np.mean(bleu2_scores))
-        bleu_3.append(np.mean(bleu3_scores))
-        bleu_4.append(np.mean(bleu4_scores))
+                    for s1, s2 in zip(original_sentences, predicted_sentences):
+                        cosine_scores.append(semantic_similarity_score([s1], [s2])[0][0]. item())
 
-    d_sr_np = np.array(args.gamma_list) * args.d
-    ticks = 0.2
+                        bleu1_scores.append(bleu_1gram(s1, s2))
+                        bleu2_scores.append(bleu_2gram(s1, s2))
+                        bleu3_scores.append(bleu_3gram(s1, s2))
+                        bleu4_scores.append(bleu_4gram(s1, s2))
+                if len(cosine_scores) > args.n_test:
+                    break
 
-    plt.figure()
-    plt.plot(d_sr_np, semantic_sim)
-    plt.grid()
-    plt.xlabel("S-R Distance")
-    plt.ylabel("Semantic Similarity")
-    # plt.xticks(np.arange(np.min(distance_np), np.max(distance_np), ticks))
-    plt.title("Semantic Similarity v. S-R Distance Ratio")
-    plt.savefig("SemanticSimilarty_v_distance.png", dpi=400)
+            mean_semantic_sim[distance_index, gamma_index] = np.mean(cosine_scores)
+            mean_bleu_1[distance_index, gamma_index] = np.mean(bleu1_scores)
+            mean_bleu_2[distance_index, gamma_index] = np.mean(bleu2_scores)
+            mean_bleu_3[distance_index, gamma_index] = np.mean(bleu3_scores)
+            mean_bleu_4[distance_index, gamma_index] = np.mean(bleu4_scores)
 
-    plt.figure()
-    plt.plot(d_sr_np, bleu_1)
-    plt.grid()
-    plt.xlabel("S-R Distance")
-    plt.ylabel("BLEU 1-gram")
-    # plt.xticks(np.arange(np.min(distance_np), np.max(distance_np), ticks))
-    plt.title("BLEU 1-gram v. S-R Distance")
-    plt.savefig("BLEU1gram_v_distance.png", dpi=400)
+            std_semantic_sim[distance_index, gamma_index] = np.std(cosine_scores, ddof=1) / np.sqrt(len(cosine_scores))
+            std_bleu_1[distance_index, gamma_index] = np.std(bleu1_scores, ddof=1) / np.sqrt(len(bleu1_scores))
+            std_bleu_2[distance_index, gamma_index] = np.std(bleu2_scores, ddof=1) / np.sqrt(len(bleu2_scores))
+            std_bleu_3[distance_index, gamma_index] = np.std(bleu3_scores, ddof=1) / np.sqrt(len(bleu3_scores))
+            std_bleu_4[distance_index, gamma_index] = np.std(bleu4_scores, ddof=1) / np.sqrt(len(bleu4_scores))
 
-    plt.figure()
-    plt.plot(d_sr_np, bleu_2)
-    plt.grid()
-    plt.xlabel("S-R Distance")
-    plt.ylabel("BLEU 2-gram")
-    # plt.xticks(np.arange(np.min(distance_np), np.max(distance_np), ticks))
-    plt.title("BLEU 2-gram v. S-R Distance")
-    plt.savefig("BLEU2gam_v_distance.png", dpi=400)
 
-    plt.figure()
-    plt.plot(d_sr_np, bleu_3)
-    plt.grid()
-    plt.xlabel("S-R Distance")
-    plt.ylabel("BLEU 3-gram")
-    # plt.xticks(np.arange(np.min(distance_np), np.max(distance_np), ticks))
-    plt.title("BLEU 3-gram v. S-R Distance")
-    plt.savefig("BLEU3gram_v_distance.png", dpi=400)
+    np.save("conventional_mean_semantic_sim.npy", mean_semantic_sim)
+    np.save("conventional_mean_bleu_1.npy", mean_bleu_1)
+    np.save("conventional_mean_bleu_2.npy", mean_bleu_2)
+    np.save("conventional_mean_bleu_3.npy", mean_bleu_3)
+    np.save("conventional_mean_bleu_4.npy", mean_bleu_4)
 
-    plt.figure()
-    plt.plot(d_sr_np, bleu_4)
-    plt.grid()
-    plt.xlabel("S-R Distance")
-    plt.ylabel("BLEU 4-gram")
-    # plt.xticks(np.arange(np.min(distance_np), np.max(distance_np), ticks))
-    plt.title("BLEU 4-gram v. S-R Distance")
-    plt.savefig("BLEU4gram_v_distance.png", dpi=400)
-
-    np.save("conventional_semantic_sim.npy", semantic_sim)
-
-    np.save("conventional_bleu_1.npy", bleu_1)
-    np.save("conventional_bleu_2.npy", bleu_2)
-    np.save("conventional_bleu_3.npy", bleu_3)
-    np.save("conventional_bleu_4.npy", bleu_4)
+    np.save("conventional_std_semantic_sim.npy", std_semantic_sim)
+    np.save("conventional_std_bleu_1.npy", std_bleu_1)
+    np.save("conventional_std_bleu_2.npy", std_bleu_2)
+    np.save("conventional_std_bleu_3.npy", std_bleu_3)
+    np.save("conventional_std_bleu_4.npy", std_bleu_4)
+    #
+    # d_sr_np = np.array(args.gamma_list) * args.d
+    #
+    # plt.figure()
+    # plt.plot(d_sr_np, mean_semantic_sim)
+    # plt.grid()
+    # plt.xlabel("S-R Distance")
+    # plt.ylabel("Semantic Similarity")
+    # plt.title("Semantic Similarity v. S-R Distance Ratio")
+    # plt.savefig("SemanticSimilarty_v_distance.png", dpi=400)
+    #
+    # plt.figure()
+    # plt.plot(d_sr_np, mean_bleu_1)
+    # plt.grid()
+    # plt.xlabel("S-R Distance")
+    # plt.ylabel("BLEU 1-gram")
+    # plt.title("BLEU 1-gram v. S-R Distance")
+    # plt.savefig("BLEU1gram_v_distance.png", dpi=400)
+    #
+    # plt.figure()
+    # plt.plot(d_sr_np, mean_bleu_2)
+    # plt.grid()
+    # plt.xlabel("S-R Distance")
+    # plt.ylabel("BLEU 2-gram")
+    # plt.title("BLEU 2-gram v. S-R Distance")
+    # plt.savefig("BLEU2gam_v_distance.png", dpi=400)
+    #
+    # plt.figure()
+    # plt.plot(d_sr_np, mean_bleu_3)
+    # plt.grid()
+    # plt.xlabel("S-R Distance")
+    # plt.ylabel("BLEU 3-gram")
+    # plt.title("BLEU 3-gram v. S-R Distance")
+    # plt.savefig("BLEU3gram_v_distance.png", dpi=400)
+    #
+    # plt.figure()
+    # plt.plot(d_sr_np, mean_bleu_4)
+    # plt.grid()
+    # plt.xlabel("S-R Distance")
+    # plt.ylabel("BLEU 4-gram")
+    # plt.title("BLEU 4-gram v. S-R Distance")
+    # plt.savefig("BLEU4gram_v_distance.png", dpi=400)
