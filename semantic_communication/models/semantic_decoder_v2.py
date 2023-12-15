d45336c0ee138979084452bf2780538c9349fd9c
enesarda <enesarda22@gmail.com>
2023-12-15 11:43:50 -0500

    add cross attention

---
+++ semantic_communication/models/semantic_decoder_v2.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+from semantic_communication.utils.general import get_device
+
+
+class MultiInputSequential(nn.Sequential):
+    def forward(self, *inputs):
+        for module in self._modules.values():
+            if type(inputs) == tuple:
+                inputs = module(*inputs)
+            else:
+                inputs = module(inputs)
+        return inputs
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, n_heads, n_embeddings, block_size):
+        super().__init__()
+        self.sa_heads = nn.MultiheadAttention(
+            embed_dim=n_embeddings,
+            num_heads=n_heads,
+            dropout=0.1,
+            batch_first=True,
+        )
+        self.ca_heads = nn.MultiheadAttention(
+            embed_dim=n_embeddings,
+            num_heads=n_heads,
+            dropout=0.1,
+            batch_first=True,
+        )
+
+        self.ff_net = nn.Sequential(
+            nn.Linear(n_embeddings, 4 * n_embeddings),
+            nn.ReLU(),
+            nn.Linear(4 * n_embeddings, n_embeddings),  # projection
+            nn.Dropout(0.1),
+        )
+        self.ln1 = nn.LayerNorm(n_embeddings)
+        self.ln2 = nn.LayerNorm(n_embeddings)
+        self.ln3 = nn.LayerNorm(n_embeddings)
+
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x, encoder_output, attention_mask):
+        # norm before the layer, residual connection after the layer
+        x_normed = self.ln1(x)
+        attention_out = self.sa_heads(
+            query=x_normed,
+            key=x_normed,
+            value=x_normed,
+            key_padding_mask=(attention_mask == 0),
+            need_weights=False,
+            attn_mask=(self.tril == 0),
+            is_causal=True,
+        )[0]
+        x = x + attention_out
+
+        x_normed = self.ln2(x)
+        attention_out = self.ca_heads(
+            query=x_normed,
+            key=encoder_output,
+            value=encoder_output,
+            key_padding_mask=(attention_mask == 0),
+            need_weights=False,
+            attn_mask=(self.tril == 0),
+            is_causal=True,
+        )[0]
+        x = x + attention_out
+
+        x = x + self.ff_net(self.ln3(x))
+        return x, attention_mask
+
+
+class SemanticDecoder(nn.Module):
+    def __init__(self, vocab_size, n_blocks, n_heads, n_embeddings, block_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embeddings)
+        self.position_embedding_table = nn.Embedding(block_size, n_embeddings)
+
+        self.decoder_blocks = MultiInputSequential(
+            *[
+                DecoderBlock(
+                    n_heads=n_heads,
+                    n_embeddings=n_embeddings,
+                    block_size=block_size,
+                )
+                for _ in range(n_blocks)
+            ]
+        )
+        self.ln = nn.LayerNorm(n_embeddings)
+        self.lm_head = nn.Linear(n_embeddings, vocab_size)
+        self.device = get_device()
+
+    def forward(self, idx, encoder_output, attention_mask=None, targets=None):
+        B, T, C = encoder_output.shape
+
+        token_embeddings = self.token_embedding_table(idx)  # (B,T,C)
+        pos_embeddings = self.position_embedding_table(
+            torch.arange(T, device=self.device)
+        )  # (T,C)
+        x = token_embeddings + pos_embeddings
+
+        if attention_mask is None:
+            attention_mask = torch.ones(B, T, dtype=torch.long).to(self.device)
+
+        x, _ = self.decoder_blocks(x, encoder_output, attention_mask)
+        logits = self.lm_head(self.ln(x))
+
+        if targets is None:
+            loss = None
+        else:
+            logits = logits.reshape(B * T, -1)
+            targets = targets.reshape(B * T)
+            attention_mask = attention_mask.flatten() == 1
+
+            loss = F.cross_entropy(logits[attention_mask, :], targets[attention_mask])
+
+        return logits, loss
+
+    def generate(self, encoder_output, attention_mask=None, sample=False):
+        B, T, C = encoder_output.shape
+
+        # get the predictions
+        logits, _ = self(encoder_output, attention_mask)  # (B, T, C)
+        # apply softmax to get probabilities
+        probs = F.softmax(logits, dim=-1)  # (B, C)
+
+        if sample:
+            idx_next = torch.multinomial(
+                probs.view(B * self.block_size, -1),
+                num_samples=1,
+            )
+            idx_next = idx_next.reshape(B, -1)
+        else:
+            idx_next = torch.argmax(probs, dim=-1)
+
+        return idx_next  # (B, T)
