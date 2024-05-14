254adc3467e67deb0deaec5b7eca8bf02bdda1f9
KutayEmrecan <72248203+KutayEmrecan@users.noreply.github.com>
2024-05-14 14:07:55 -0400

    Update baseline models

--- semantic_communication/utils/general.py
+++ semantic_communication/utils/general.py
@@ -22,6 +22,11 @@ def get_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def round_to_nearest_even(num):
+    rounded = round(num)
+    return rounded if rounded % 2 == 0 else rounded + 1 if num >= rounded else rounded - 1
+
+
 def print_loss(losses, group):
     mean_loss = np.mean(losses)
     se = np.std(losses, ddof=1) / np.sqrt(len(losses))
