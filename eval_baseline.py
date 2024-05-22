591381d9d7198cd6a725faae7903824b515fd84c
enesarda <enesarda22@gmail.com>
2024-05-22 12:40:53 -0400

    change eval_baseline script

--- eval_baseline.py
+++ eval_baseline.py
@@ -11,9 +11,9 @@ from semantic_communication.utils.general import (
     add_channel_model_args,
     add_data_args,
     load_model,
-    round_to_nearest_even
+    round_to_nearest_even,
 )
-from semantic_communication.models.baseline_models import Tx_Relay, Tx_Relay_Rx
+from semantic_communication.models.baseline_models import Tx_Relay
 from semantic_communication.data_processing.data_handler import DataHandler
 from semantic_communication.utils.channel import init_channel
 from semantic_communication.models.semantic_encoder import SemanticEncoder
@@ -37,25 +37,30 @@ def semantic_similarity_score(target_sentences, received_sentences):
     completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system",
-             "content": "You are skilled in evaluating how similar the two sentences are. Provide a number between -1 "
-                        "and 1 denoting the semantic similarity score for given sentences A and B with precision "
-                        "0.01. 1 means they are perfectly similar and -1 means they are opposite while 0 means their "
-                        "meanings are uncorrelated. Just provide a score without any words or symbols."},
-            {"role": "user", "content": f"A=({target_sentences})  B=({received_sentences})"}
-        ]
+            {
+                "role": "system",
+                "content": "You are skilled in evaluating how similar the two sentences are. Provide a number between -1 "
+                "and 1 denoting the semantic similarity score for given sentences A and B with precision "
+                "0.01. 1 means they are perfectly similar and -1 means they are opposite while 0 means their "
+                "meanings are uncorrelated. Just provide a score without any words or symbols.",
+            },
+            {
+                "role": "user",
+                "content": f"A=({target_sentences})  B=({received_sentences})",
+            },
+        ],
     )
 
     if completion.choices[0].finish_reason == "stop":
-        pattern = re.compile(r'(?<![\d.-])-?(?:0(?:\.\d+)?|1(?:\.0+)?)(?![\d.])')
+        pattern = re.compile(r"(?<![\d.-])-?(?:0(?:\.\d+)?|1(?:\.0+)?)(?![\d.])")
         res = pattern.findall(completion.choices[0].message.content)
         if len(res) == 1:
             return float(res[0])
         else:
             print(res)
-            return float('nan')
+            return float("nan")
     else:
-        return float('nan')
+        return float("nan")
 
 
 if __name__ == "__main__":
@@ -63,7 +68,6 @@ if __name__ == "__main__":
 
     # model args
     parser.add_argument("--tx-relay-path", type=str)
-    parser.add_argument("--tx-relay-rx-path", type=str)
     parser.add_argument("--API-KEY", type=str)  # API KEY
 
     add_semantic_decoder_args(parser)
@@ -105,7 +109,7 @@ if __name__ == "__main__":
 
     mean_sentence_len = np.mean(sentence_lengths)
     print(f"Average token count: {mean_sentence_len}")
-    sentence_embedding_dim = 64*5
+    sentence_embedding_dim = 64 * 5
     latent_dim = round_to_nearest_even(sentence_embedding_dim / mean_sentence_len)
     print(f"Latent dim: {latent_dim}")
 
@@ -121,15 +125,6 @@ if __name__ == "__main__":
         entire_network_train=1,
     ).to(device)
     load_model(tx_relay_model, args.tx_relay_path)
-
-    tx_relay_rx_model = Tx_Relay_Rx(
-        nin=num_classes,
-        n_emb=args.channel_block_input_dim,
-        n_latent=latent_dim,
-        channel=channel,
-        tx_relay_model=tx_relay_model,
-    ).to(device)
-    load_model(tx_relay_rx_model, args.tx_relay_rx_path)
 
     n_d = len(args.d_list)
     n_gamma = len(args.gamma_list)
@@ -157,7 +152,7 @@ if __name__ == "__main__":
             d_sr = d_sd * gamma
             d_rd = d_sd - d_sr
 
-            tx_relay_rx_model.eval()
+            tx_relay_model.eval()
 
             for b in data_handler.test_dataloader:
                 encoder_idx = b[0].to(device)
@@ -166,8 +161,8 @@ if __name__ == "__main__":
 
                 B, T = encoder_idx.shape
                 with torch.no_grad():
-                    logits, _ = tx_relay_rx_model(
-                        encoder_idx[:, 1:], encoder_attention_mask[:, 1:], d_sd, d_sr, d_rd
+                    logits, _ = tx_relay_model(
+                        encoder_idx[:, 1:], encoder_attention_mask[:, 1:], d_sr
                     )
                     probs = F.softmax(logits, dim=-1)
                     predicted_ids = (torch.argmax(probs, dim=-1)).reshape(
@@ -201,15 +196,24 @@ if __name__ == "__main__":
 
                     for s1, s2 in zip(original_sentences, predicted_sentences):
                         sim_score = semantic_similarity_score(s1, s2)
-                        bleu_1_score = sentence_bleu([word_tokenize(s1)], word_tokenize(s2), weights=[1, 0, 0, 0],
-                                                     smoothing_function=smoothing_function)
-                        bleu_score = sentence_bleu([word_tokenize(s1)], word_tokenize(s2),
-                                                   smoothing_function=smoothing_function)
+                        bleu_1_score = sentence_bleu(
+                            [word_tokenize(s1)],
+                            word_tokenize(s2),
+                            weights=[1, 0, 0, 0],
+                            smoothing_function=smoothing_function,
+                        )
+                        bleu_score = sentence_bleu(
+                            [word_tokenize(s1)],
+                            word_tokenize(s2),
+                            smoothing_function=smoothing_function,
+                        )
                         cosine_scores.append(sim_score)
                         bleu1_scores.append(bleu_1_score)
                         bleu_scores.append(bleu_score)
 
-                        records.append([d_sd, gamma, s1, s2, sim_score, bleu_1_score, bleu_score])
+                        records.append(
+                            [d_sd, gamma, s1, s2, sim_score, bleu_1_score, bleu_score]
+                        )
 
                 if len(bleu1_scores) >= args.n_test:
                     break
@@ -221,9 +225,15 @@ if __name__ == "__main__":
             mean_bleu_1[distance_index, gamma_index] = np.mean(bleu1_scores)
             mean_bleu[distance_index, gamma_index] = np.mean(bleu_scores)
 
-            std_semantic_sim[distance_index, gamma_index] = np.std(cosine_scores, ddof=1) / np.sqrt(n_test_samples)
-            std_bleu_1[distance_index, gamma_index] = np.std(bleu1_scores, ddof=1) / np.sqrt(n_test_samples)
-            std_bleu[distance_index, gamma_index] = np.std(bleu_scores, ddof=1) / np.sqrt(n_test_samples)
+            std_semantic_sim[distance_index, gamma_index] = np.std(
+                cosine_scores, ddof=1
+            ) / np.sqrt(n_test_samples)
+            std_bleu_1[distance_index, gamma_index] = np.std(
+                bleu1_scores, ddof=1
+            ) / np.sqrt(n_test_samples)
+            std_bleu[distance_index, gamma_index] = np.std(
+                bleu_scores, ddof=1
+            ) / np.sqrt(n_test_samples)
 
             np.save("ae_conventional_mean_semantic_sim.npy", mean_semantic_sim)
             np.save("ae_conventional_mean_bleu_1.npy", mean_bleu_1)
@@ -233,5 +243,16 @@ if __name__ == "__main__":
             np.save("ae_conventional_std_bleu_1.npy", std_bleu_1)
             np.save("ae_conventional_std_bleu.npy", std_bleu)
 
-            df = pd.DataFrame(records, columns=['d_sd', "Gamma", 'Sentence 1', 'Sentence 2', 'Semantic Similarity Score', 'BLEU 1 Gram Score', 'BLEU Score'])
-            df.to_excel('baseline_output.xlsx', index=False)
+            df = pd.DataFrame(
+                records,
+                columns=[
+                    "d_sd",
+                    "Gamma",
+                    "Sentence 1",
+                    "Sentence 2",
+                    "Semantic Similarity Score",
+                    "BLEU 1 Gram Score",
+                    "BLEU Score",
+                ],
+            )
+            df.to_excel("baseline_output.xlsx", index=False)
